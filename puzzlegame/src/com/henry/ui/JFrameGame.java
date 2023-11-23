c347f4b699dd664d2774eb4cd0c4c15aff3305c0
Davch <976604851@qq.com>
2023-11-23 22:21:35 +0800

    这个是本地仓master分支的上传

--- puzzlegame/src/com/henry/ui/JFrameGame.java
+++ puzzlegame/src/com/henry/ui/JFrameGame.java
@@ -7,5 +7,6 @@ public class JFrameGame extends JFrame {
     public JFrameGame(){
         this.setSize(603,680);
         this.setVisible(true);
+        System.out.println("这是本地仓的master分支");
     }
 }
