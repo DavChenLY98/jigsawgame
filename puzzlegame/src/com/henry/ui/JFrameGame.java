a8f4036216fa16e70f145509d80b5b81b1b4436a
Davch <976604851@qq.com>
2023-11-30 23:30:23 +0800

    添加了键盘监听组件，添加一键查看原图，一件通关功能。

--- puzzlegame/src/com/henry/ui/JFrameGame.java
+++ puzzlegame/src/com/henry/ui/JFrameGame.java
@@ -13,6 +13,7 @@ public class JFrameGame extends JFrame implements KeyListener {
     //记录空白方块在data中的位置
     int x = 0;
     int y = 0;
+    String path = "puzzlegame/image/animal/animal3/";
 
     public JFrameGame() {
         //初始化界面
@@ -54,12 +55,13 @@ public class JFrameGame extends JFrame implements KeyListener {
 
     private void initImage() {
         //细节：先加载的图片在上方，后加载的图片在下方。
-
+        //清楚已有的图片
+        this.getContentPane().removeAll();
 
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
                 //创建一个管理容器存储图片对象
-                JLabel jLabel = new JLabel(new ImageIcon("puzzlegame/image/animal/animal3/"
+                JLabel jLabel = new JLabel(new ImageIcon(path
                         + data[i][j] + ".jpg"));
                 //将管理容器放置在指定坐标位置
                 jLabel.setBounds(105 * j + 83, 105 * i + 134, 105, 105);
@@ -78,6 +80,8 @@ public class JFrameGame extends JFrame implements KeyListener {
         background.setBounds(40, 40, 508, 560);
         this.getContentPane().add(background);
 
+
+        this.getContentPane().repaint();
 
     }
 
@@ -131,6 +135,24 @@ public class JFrameGame extends JFrame implements KeyListener {
 
     @Override
     public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == 65) {
+            this.getContentPane().removeAll();
+            JLabel jLabel = new JLabel(new ImageIcon(path + "all.jpg"));
+            jLabel.setBounds(83, 134, 420, 420);
+            //这句话不知道为什么无法正常显示图片位置
+//        jLabel.setLocation(0,0);
+            this.getContentPane().add(jLabel);
+            //添加背景边框
+            ImageIcon bg = new ImageIcon("puzzlegame/image/background.png");
+            //利用容器存储图片对象
+            JLabel background = new JLabel(bg);
+//        JLabel background =new JLabel(new ImageIcon("puzzlegame/image/background.png"));
+            //设置背景对象对的边框大小和位置
+            background.setBounds(40, 40, 508, 560);
+            this.getContentPane().add(background);
+
+            this.getContentPane().repaint();
+        }
 
     }
 
@@ -140,21 +162,53 @@ public class JFrameGame extends JFrame implements KeyListener {
         //左：37 上：38 右：39 下：40
         int code = e.getKeyCode();
         if (code == 37) {
-            int temp = data[x][y];
+            //如果已经靠左边界
+            if (y == 0) {
+                return;
+            }
             data[x][y] = data[x][y - 1];
-            data[x][y - 1] = temp;
+            data[x][y - 1] = 0;
+            y--;
+            initImage();
         } else if (code == 38) {
-            int temp = data[x][y];
+            //已经靠上边界
+            if (x == 0) {
+                return;
+            }
             data[x][y] = data[x - 1][y];
-            data[x - 1][y] = temp;
+            data[x - 1][y] = 0;
+            x--;
+            initImage();
         } else if (code == 39) {
-            int temp = data[x][y];
+            //已经靠右边界
+            if (y == 3) {
+                return;
+            }
             data[x][y] = data[x][y + 1];
-            data[x][y + 1] = temp;
+            data[x][y + 1] = 0;
+            y++;
+            initImage();
         } else if (code == 40) {
-            int temp = data[x][y];
+            //已经靠下边界
+            if (x == 3) {
+                return;
+            }
             data[x][y] = data[x + 1][y];
-            data[x + 1][y] = temp;
+            data[x + 1][y] = 0;
+            x++;
+            initImage();
+        } else if (code == 65) {    //当松开a键时，图片重新转换为分割后的随即图片
+            initImage();
+        } else if (code == 87) {
+            data = new int[][]{
+                    {1, 2, 3, 4},
+                    {5, 6, 7, 8},
+                    {9, 10, 11, 12},
+                    {13, 14, 15, 0}
+            };
+            initImage();
+            x = 3;
+            y = 3;
         }
     }
 }
